77ba0ee83b25f11f514254beb32854265d22edaf
Denis Lachance <dudeofea@hotmail.com>
2017-09-10 20:13:59 -0600

    fixed issue with res deleting themselves

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $(window).load(function(){
 	var grid_container = $("#grid");
-	var height = 20;
-	var width = 20;
-	var tile_size = 20;
+	var height = 30;
+	var width = 30;
+	var tile_size = 30;
 	for (var i = 0; i < height; i++) {
 		for (var j = 0; j < width; j++) {
 			var style = 'style="top: '+i*tile_size+'px;left: '+j*tile_size+'px;height: '+tile_size+'px;width: '+tile_size+'px;"';
@@ -29,26 +29,49 @@ function add_atom_onclick(e){
 }
 //add an atom to an element (deletes whatever's currently there)
 function add_atom(e, fn){
-	if(!e){ return; }
+	if(!e || !fn){ return; }
 	//set color
 	$(e).css('background-color', fn.color);
 	//run atom's function
 	clearTimeout(e.timeout);
+	e.fn = fn;
 	e.timeout = setTimeout(fn.bind(null, e, function(){
-		//reset element
-		$(e).css('background-color', "");
-		e.timeout = null;
+		add_atom(e, e.fn);
 	}), latency);
 }
-//copy atom to a random element around them
-function copy_random(e, fn){
+//clear an element of anything in it
+function delete_atom(e){
+	if(!e){ return; }
+	clearTimeout(e.timeout);
+	e.fn = null;
+	e.timeout = null;
+	$(e).css('background-color', '');
+}
+//pick a random direction around us, can be null
+function pick_random(e){
 	var directions = ["top", "bottom", "left", "right"];
-	var next = e[directions[Math.floor(Math.random() * directions.length)]];
-	if(next && next.timeout == null){
-		add_atom(next, fn);
-	}else{
-		add_atom(e, fn);
+	return e[directions[Math.floor(Math.random() * directions.length)]];
+}
+//move to a random spot, if you can
+function move_random(e){
+	var sel = pick_random(e);
+	if(sel && sel.timeout == null){
+		add_atom(sel, e.fn);
+		delete_atom(e);
 	}
+}
+//randomly delete a random element around you
+function delete_random(e){
+	var sel = pick_random(e);
+	if(sel){
+		clearTimeout(sel.timeout);
+		$(sel).css('background-color', "");
+		sel.timeout = null;
+	}
+}
+//create an atom in a random spot around you
+function create_random(e, fn){
+	add_atom(pick_random(e), fn);
 }
 //returns true p percent of the time (p is from 0-1)
 function prob(p){
@@ -57,19 +80,22 @@ function prob(p){
 
 //moves randomly, does nothing
 function resource(e, callback){
-	copy_random(e, resource);
+	move_random(e);
 	callback();
 }
 resource.color = "#50514F";
 //spawns resource, sometimes duplicates
 function regulator(e, callback){
-	if(prob(0.2)){
-		copy_random(e, resource);
-	}
 	if(prob(0.1)){
-		copy_random(e, regulator);
+		create_random(e, resource);
 	}
-	copy_random(e, regulator);
+	if(prob(0.005)){
+		create_random(e, regulator);
+	}
+	if(prob(0.05)){
+		delete_random(e);
+	}
+	move_random(e);
 	callback();
 }
 regulator.color = "#5BC3EB";
